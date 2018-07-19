c648f6daaa9cd80b14b75a6febd09771aaf06d61
Chris Franklin <cf73021@gmail.com>
2018-07-19 15:24:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.length == 1
     array = array.join
   elsif array.length == 3
-    array[1] = array[1] + " and "
-    array = array.join
+    array[1] = array[1] + " and"
+    array = array.join(" ")
   end
   return array
 end
