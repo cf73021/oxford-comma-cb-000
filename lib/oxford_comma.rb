89cad88988236038a59407cb1a0a4ccdb045c539
Chris Franklin <cf73021@gmail.com>
2018-07-19 15:27:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     array = array.join(" and ")
   elsif array.length == 1
     array = array.join
-  elsif array.length == 3
-    array[2] = "and " + array[2] 
+  else
+    array[-1] = "and " + array[-1]
     array = array.join(", ")
   end
   return array
